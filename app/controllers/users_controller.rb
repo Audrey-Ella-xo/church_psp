2e29e29af4a325d866e7cb64f93e2a2cd6bc4370
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-08-18 19:29:01 +0100

    Add user controller and index view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show edit update destroy ]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+        # respond_to do |format|
+        #     if @user.update(user_params)
+        #       format.html { redirect_to user_steps_path, notice: "User was successfully updated." }
+        #       format.json { render :show, status: :ok, location: @user }
+        #     else
+        #       format.html { render :edit, status: :unprocessable_entity }
+        #       format.json { render json: @user.errors, status: :unprocessable_entity }
+        #     end
+        #   end
+    end
+
+    def destroy
+    end
+
+    private
+
+    # def user_params
+    #     params.require(:user).permit(:first_name, :middle_name, :last_name, :age, :gender, :email, :dob, :marriage_status, :umbrella_boddy_id, address: [:street_address, :city, :state, :zip])
+    #   end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+end
